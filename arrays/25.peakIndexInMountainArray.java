0d4ccf116b66f80ea3a42438ece9e144d88e3982
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2025-12-19 06:40:16 +0530

    Create 25.peakIndexInMountainArray.java

---
+++ arrays/25.peakIndexInMountainArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int low=0,high=n-1;
+        int ans=n;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}
